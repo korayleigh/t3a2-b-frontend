39a1bf23ffec7d90d636eb964ee3990c3a246583
leighpoulier <86460742+leighpoulier@users.noreply.github.com>
2022-02-04 23:31:27 +1100

    set api url depending on environment

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
+const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://mexiquito.herokuapp.com/api';
+
 const mexiquitoApi = axios.create({
-  baseURL: 'http://localhost:3000/api'
-})
+  baseURL: API_URL
+});
 
 mexiquitoApi.interceptors.request.use((request) => {
   const jwt = sessionStorage.getItem('jwt')
