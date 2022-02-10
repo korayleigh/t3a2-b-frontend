bbd6fa8f43dc4746a45e7477d2bf14fbb920c08a
leighpoulier <86460742+leighpoulier@users.noreply.github.com>
2022-02-10 13:20:50 +1100

    select api url based on frontend location in production

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
+console.log(window.location.hostname);
+const location_regex = /^https?:\/\/deploy-preview-(\d+)--korayleigh.netlify.app(\/.*)$/;
+const hostname_regex = /^deploy-preview-(\d+)--korayleigh.netlify.app$/;
+
+if (window.location.hostname.match(location_regex)) {
+  console.log('matched deploy preview location');
+}
+
+if (window.location.hostname.match(hostname_regex)) {
+  console.log('matched deploy preview hostname');
+}
+
 const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://mexiquito.herokuapp.com/api';
 
 const mexiquitoApi = axios.create({
