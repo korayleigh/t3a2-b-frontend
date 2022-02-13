6024da6256ae686125c2eed98db8a808b7d52ad1
koray-eren <koray.eren@outlook.com>
2022-02-13 20:23:32 +1100

    linter fixes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CartProvider } from 'react-use-cart';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
